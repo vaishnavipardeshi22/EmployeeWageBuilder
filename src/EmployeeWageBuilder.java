2b61e1b12eb64d604e2c0a38bec5b3997d34ec3c
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-11 16:19:16 +0530

    ADDED : Welcome message

---
+++ src/EmployeeWageBuilder.java
@@ -0,0 +1,7 @@
+public class EmployeeWageBuilder
+{
+    public static void main(String[] args)
+    {
+        System.out.println("******************** WELCOME TO EMPLOYEE WAGE COMPUTATION ********************");
+    }
+}
