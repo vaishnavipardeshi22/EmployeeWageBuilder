3885b55c72435b5360695ca97a475b59d292ae8c
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-11 16:42:41 +0530

    ADDED : Computation for full time employee wage

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -5,13 +5,21 @@ public class EmployeeWageBuilder
         System.out.println("******************** WELCOME TO EMPLOYEE WAGE COMPUTATION ********************");
 
         //CONSTANT
-        int IS_PRESENT=1;
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
 
-        //COMPUTING EMPLOYEE ATTENDANCE
+        //VARIABLES
+        int empHrs=0;
+        int empWage=0;
+
+        //CALCULATE DAILY EMPLOYEE WAGE
         double empCheck=Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee is present.");
+        if (empCheck == IS_FULL_TIME)
+            empHrs=8;
         else
-            System.out.println("Employee is absent.");
+            empHrs=0;
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee wage : " + empWage);
     }
 }
